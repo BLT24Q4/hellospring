12c6e3d655c482b8406671e0d85e489034a6d971
Sean Nam <blt24q4@gmail.com>
2025-01-13 16:05:48 +0900

    다양한 요청 처리

--- src/main/java/himedia/hellospring/controllers/HelloController.java
+++ src/main/java/himedia/hellospring/controllers/HelloController.java
@@ -1,9 +1,14 @@
 package himedia.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import himedia.hellospring.vo.RequestVo;
 
 //	POJO
 @Controller
@@ -23,5 +28,37 @@ public class HelloController {
 		mav.setViewName("/WEB-INF/views/hello.jsp");	//	뷰 정보 설정
 		
 		return mav;
+	}
+	
+	//	요청 파라미터를 객체 담아서 전달
+	//	?name=Spring&no=10
+	@GetMapping("/using-vo")
+//	public ModelAndView usingVo(
+//			@RequestParam("name") String name,
+//			@RequestParam("no") Long no) {
+	public ModelAndView usingVo(
+			@ModelAttribute RequestVo vo) {
+		
+		ModelAndView mav = new ModelAndView();
+//		mav.addObject("message", "Name: " + name +
+//				", No: " + no);
+		mav.addObject("message", "Name: " + vo.getName() + 
+				", No: " + vo.getNo());
+		
+		mav.setViewName("/WEB-INF/views/hello.jsp");
+		
+		return mav;
+	}
+	
+	//	요청 처리 메서드의 Return Type이 String -> View Name을 의미
+	@GetMapping("/rstring")
+	public String returnString() {
+		return "/WEB-INF/views/hello.jsp";
+	}
+
+	@GetMapping("/rstring2")
+	public String returnString2(Model model) {
+		model.addAttribute("message", "rstring2");
+		return "/WEB-INF/views/hello.jsp";
 	}
 }
